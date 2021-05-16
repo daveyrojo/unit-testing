923d1d5926baee5623d80995160c8f9f559994f3
david eldridge <d.vincenteldridge@gmail.com>
2021-05-15 21:40:51 -0400

    made more complext function to test

--- util.js
+++ util.js
@@ -3,4 +3,13 @@ const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 const mult = (a, b) => a * b;
 
+function moreAdvMath(a, b) {
+    if (a != b) {
+        return a + b;
+    } else {
+        let c = a + b;
+        return a + b - c;
+    }
+};
+
 module.exports = {add, sub, div, mult};
